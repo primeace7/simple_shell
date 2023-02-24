9c64df888d9e418f5f17fa7d0d92b1eda11773d5
primeace7 <osaruonamenjeffery@gmail.com>
2023-02-24 16:37:37 +0000

    Readd project files

---
+++ helper_functions2.c
@@ -0,0 +1,28 @@
+#include "shell.h"
+
+
+/**
+ * fork_child - fork a child process to execute input command
+ * Return: the pid of the forked child, or -1 on error
+ */
+
+pid_t fork_child(void)
+{
+	pid_t child_fork;
+
+	child_fork = fork();
+	return (child_fork);
+}
+
+/**
+ * display_prompt - display a shell prompt ($) and wait for input
+ *
+ * Return: nothing
+ *
+ */
+
+void display_prompt(void)
+{
+
+	printf("#cisfun$ ");
+}
